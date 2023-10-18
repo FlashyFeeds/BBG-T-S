e84b60aedc0873b60e1cc46a6509e0da432b336f
FlashyFeeds <popanuilia@gmail.com>
2023-10-18 10:13:45 +0200

    updt

--- config/config.sql
+++ config/config.sql
@@ -10,9 +10,9 @@ INSERT INTO Parameters(ParameterId, Name, Description, Domain, DefaultValue, Con
     ('BBGTS_GameMode', 'LOC_BBGTS_GAMEMODE_NAME', 'LOC_BBGTS_GAMEMODE_DESCRIPTION', 'bool', '1', 'Game', 'BBGTS_GAMEMODE', 'GameModes', '1', '1', '-10'),
     ('BBGTS_DebugLua', 'LOC_BBGTS_DEBUG_LUA_NAME', 'LOC_BBGTS_DEBUG_LUA_DESC', 'bool', '1', 'Game', 'BBGTS_DEBUG_LUA', 'AdvancedOptions','1', '1', '20010'),
     ('BBGTS_MP_CHEATS', 'LOC_BBGTS_MP_CHEAT_NAME', 'LOC_BBGTS_MP_CHEAT_DESC', 'bool', '0', 'Game', 'BBGTS_MP_CHEATS', 'AdvancedOptions','1', '1', '20020'),
-    ('BBGTS_MPTestSpy', 'LOC_BBGTS_TEST_SPY_NAME', 'LOC_BBGTS_TEST_SPY_DESC', 'bool', '0', 'Game', 'BBGTS_TEST_SPY', 'AdvancedOptions','1', '1', '20030'),
-    ('BBGTS_STRATEGICS', 'LOC_BBGTS_STRATEGICS_NAME', 'LOC_BBGTS_STRATEGICS_DESC', 'bool', '0', 'Game', 'BBGTS_STRATEGICS', 'AdvancedOptions','1', '1', '20040'),
-    ('BBGTS_TURN_PROCESSING', 'LOC_BBGTS_TURN_PROCESSING_NAME', 'LOC_BBGTS_TURN_PROCESSING_DESC', 'bool', '0', 'Game', 'BBGTS_TURN_PROCESSING', 'AdvancedOptions','1', '1', '20050');
+    --('BBGTS_MPTestSpy', 'LOC_BBGTS_TEST_SPY_NAME', 'LOC_BBGTS_TEST_SPY_DESC', 'bool', '0', 'Game', 'BBGTS_TEST_SPY', 'AdvancedOptions','1', '1', '20030'),
+    ('BBGTS_STRATEGICS', 'LOC_BBGTS_STRATEGICS_NAME', 'LOC_BBGTS_STRATEGICS_DESC', 'bool', '0', 'Game', 'BBGTS_STRATEGICS', 'AdvancedOptions','1', '1', '20040');
+    --('BBGTS_TURN_PROCESSING', 'LOC_BBGTS_TURN_PROCESSING_NAME', 'LOC_BBGTS_TURN_PROCESSING_DESC', 'bool', '0', 'Game', 'BBGTS_TURN_PROCESSING', 'AdvancedOptions','1', '1', '20050');
     --('BBGTS_ProductionPanel_BBG', 'LOC_BBGTS_PRODUCTION_PANEL_NAME', 'LOC_BBGTS_BBGTS_PRODUCTION_PANEL_DESC', 'bool', '1', 'Game', 'BBGTS_PRODUCTION_PANEL_UTIL', 'AdvancedOptions','1', '1', '20020');--
 
 --Insert your Bug Fixes Here, sort order between 20020 and 30000
@@ -52,8 +52,8 @@ INSERT INTO Parameters(ParameterId, Name, Description, Domain, DefaultValue, Con
 
 INSERT INTO ParameterCriteria(ParameterId, ConfigurationGroup, ConfigurationId, Operator, ConfigurationValue) VALUES
     ('BBGTS_DebugLua', 'Game', 'BBGTS_GAMEMODE', 'Equals', '1'),
-    ('BBGTS_MP_CHEATS', 'Game', 'BBGTS_GAMEMODE', 'Equals', '1'), 
-    ('BBGTS_MPTestSpy', 'Game', 'BBGTS_GAMEMODE', 'Equals', '1');
+    ('BBGTS_MP_CHEATS', 'Game', 'BBGTS_GAMEMODE', 'Equals', '1');
+    --('BBGTS_MPTestSpy', 'Game', 'BBGTS_GAMEMODE', 'Equals', '1');
     --('BBGTS_Amani', 'Game', 'BBGTS_GAMEMODE', 'Equals', '1'),
     --('BBGTS_Communism_Mod', 'Game', 'BBGTS_GAMEMODE', 'Equals', '1'),
     --('BBGTS_Communism_Build', 'Game', 'BBGTS_GAMEMODE', 'Equals', '1'),
